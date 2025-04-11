a9e095b54e28daadc92208baaceb994252ac65b0
Chintan Mathukiya <chintance1993@gmail.com>
2025-04-12 01:43:16 +0530

    hide name

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -75,7 +75,7 @@
                 </div>
             </div>
             <form @submit.prevent="submitQuiz" class="space-y-8">
-                <div class="max-w-md mx-auto my-8 bg-white rounded-3xl shadow-lg p-6 hover:shadow-xl transition-shadow duration-300">
+                <div class="d-none hidden max-w-md mx-auto my-8 bg-white rounded-3xl shadow-lg p-6 hover:shadow-xl transition-shadow duration-300">
                     <label for="name" class="block text-lg font-medium text-gray-700 mb-2">Enter your name:</label>
                     <input
                         v-model="name"
